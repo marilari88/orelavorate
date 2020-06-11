49cb8438e0e78638ace8d181f5e5be344f41c70f
Marco Ilari <marilari88@gmail.com>
2020-06-12 00:23:16 +0200

    Eseguite modfiche per adeguament ad api rest

--- components/organisms/ElencoTimbrature.js
+++ components/organisms/ElencoTimbrature.js
@@ -6,6 +6,7 @@ class ElencoTimbrature extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      caricamento: true,
       elencoTimbrature: [],
     };
   }
@@ -17,20 +18,30 @@ class ElencoTimbrature extends React.Component {
   caricaElencoTimbrature = async () => {
     await axios
       .get(process.env.NEXT_PUBLIC_API_URL + "/timbratura")
-      .then((timbrature) => timbrature.data)
       .then((timbrature) => {
-        this.setState({ elencoTimbrature: timbrature });
+        this.setState({
+          elencoTimbrature: timbrature.data.elenco,
+          caricamento: false,
+        });
       });
   };
 
   mostraElencoTimbrature = () => {
+    console.log(this.state.elencoTimbrature);
     return Array.from(this.state.elencoTimbrature).map((timbratura) => {
       return <RigaTimbratura key={timbratura.id} timbratura={timbratura} />;
     });
   };
 
   render() {
-    return <>{this.mostraElencoTimbrature()}</>;
+    return (
+      <>
+        {this.mostraElencoTimbrature()}
+        <div className="messaggioCaricamento">
+          {this.state.caricamento ? "Caricamento In corso" : ""}
+        </div>
+      </>
+    );
   }
 }
 
