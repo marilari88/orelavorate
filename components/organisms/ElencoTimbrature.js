b72938dd9aeed39eed6c3a50cd86921a60b6a7fd
Marco Ilari <marco@pop-os.localdomain>
2020-08-26 01:18:45 +0200

    Aggiunta catch per la gestione degli errori di connessione in elencotimbrature

--- components/organisms/ElencoTimbrature.js
+++ components/organisms/ElencoTimbrature.js
@@ -24,6 +24,7 @@ class ElencoTimbrature extends React.Component {
             elencoTimbrature: timbrature.data.elenco,
           });
       })
+      .catch((err) => console.log(err.message))
       .finally(
         this.setState({
           caricamento: false,
